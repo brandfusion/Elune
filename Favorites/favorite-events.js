41c87b4806b2f904e4e29669e2cc53fb3ba5f763
Daniel Voicu <daniel.voicu@brandfusion.ro>
2017-09-18 23:24:46 +0300

    added favorite
    favorite module WIP
    - needs to get data from API + render

---
+++ Favorites/favorite-events.js
@@ -0,0 +1,149 @@
+let buttonsFavorite = document.querySelectorAll('button.favorite-trigger');
+
+buttonsFavorite.forEach(el => {
+  el.addEventListener("click", function(e) { 
+    e.stopPropagation();          
+    triggerFavoriteAction(e);    
+  });
+});
+sessionStorage.clear();
+function triggerFavoriteAction(el) { 
+  el.stopPropagation();   
+  let action= "";
+  let message= "";
+  let type ="";
+  // console.log(((el.target.className).split(" ").filter(val => val === "active")).length);
+  if(((el.target.className).split(" ").filter(val => val === "active")).length === 0) {
+    action = el.target.attributes["data-addfavorite"].value;
+    message = el.target.attributes["data-addtext"].value;
+    type = "add";
+  } else {
+    action =  el.target.attributes["data-removefavorite"].value;
+    message = el.target.attributes["data-removetext"].value;
+    type = "remove";
+  }  
+  
+  let id = el.target.attributes["data-id"].value;
+  let name = el.target.attributes["data-name"].value;
+  let url = el.target.attributes["data-url"].value;
+  let code = el.target.attributes["data-code"].value;
+  let img = el.target.attributes["data-img"].value;
+  let item = {id,name,url,code,img}
+  obj = {action, message, type, item }
+  
+  fakeAjaxCall(obj).then(function(data){
+    let message = data.message;  
+
+    if(data.type === "add") {
+      (Favorite.addItem(data.item)).then((data) => { 
+          // __crudeRender(data);           
+          __markAdded(el);
+          __updateCount((data.payload).length);  
+          alert(message);        
+      }).catch((error) => { 
+          alert(error.message);
+          // console.log(new Error(error.message))
+      }); 
+
+    } else if(data.type="remove") {
+      (Favorite.removeItem(data.item)).then((data) => {          
+          // __crudeRender(data); 
+          __markRemoved(el);         
+          __updateCount((data.payload).length);           
+          alert(message);           
+      }).catch((error) => { 
+          alert(error.message);
+          // console.log(new Error(error.message))
+      }); 
+    }
+
+  });
+  
+}
+
+function fakeAjaxCall(obj){  
+  return new Promise(function(resolve){
+      setTimeout(function(){
+          resolve(obj);
+      }, 1000);
+  });
+}
+
+function __markAdded(el) {
+  el.target.className = [...(el.target.className).split(" "), "active"].join(" ");
+  el.target.innerHTML = "Remove";
+}
+function __markRemoved(el) {
+  el.target.className = (el.target.className).split(" ").filter(val => val !== "active").join(" ");
+  el.target.innerHTML = "Add";
+}
+function __updateCount(val) {  
+  document.getElementById("favorite-counter").innerHTML = val;
+}
+
+function __crudeRender(data){ 
+  let arr = data.payload; 
+  let output = `<div id="test">`;  
+    arr.map(o => {
+      output += `<div class="item">
+        <div class="name">${o.name}</div>
+        <button class="close" data-id="${o.id}">X</button>
+        <div class="image"><img src="${o.img}" title="${o.name}" width="75" height="75" /></div>
+      </div>`
+    });
+  
+      
+  output += '</div>';
+  document.getElementById("result").innerHTML = output;
+
+  __crudeRenderEvents();
+  return output;
+}
+
+function __crudeRenderEvents(){
+  let buttonsClose = document.querySelectorAll("button.close");  
+  buttonsClose.forEach(el => {
+    el.addEventListener("click", function(e){
+      removeFavoriteItem(e);
+    });
+  });
+}
+
+function removeFavoriteItem(e) {
+  e.preventDefault(); 
+  id = e.target.attributes["data-id"].value;
+  // name = e.target.attributes["data-name"].value;
+  // url = e.target.attributes["data-url"].value;
+  // code = e.target.attributes["data-code"].value;
+  // img = e.target.attributes["data-img"].value;
+  
+  let obj = {id};
+  (Favorite.removeItem(obj))
+  .then((data) => { 
+    __crudeRender(data);
+    __updateCount((data.payload).length);     
+  })            
+  .catch((error) => {
+      alert(error.message);
+      // console.log(new Error(error))
+  });  
+}
+
+// function addCompareItem(e) {
+//   e.preventDefault(); 
+//   id = e.target.attributes["data-id"].value;
+//   name = e.target.attributes["data-name"].value;
+//   url = e.target.attributes["data-url"].value;
+//   code = e.target.attributes["data-code"].value;
+//   img = e.target.attributes["data-img"].value;
+
+//   let obj = {name,id,code,img,url};        
+
+//   (Compare.addItem(obj)).then((data) => {          
+//       // alert(data.message);      
+//       __crudeRender(data);
+//   }).catch((error) => { 
+//       alert(error.message);
+//       // console.log(new Error(error.message))
+//   }); 
+// }
---
+++ Favorites/favorite.js
@@ -0,0 +1,58 @@
+const Favorite = {
+  data: [],
+  _itemAlreadyAdded: function(obj) {            
+    return ((this.data).filter(o => o.id === obj.id )).length != 0 ? true : false;
+  },
+  addItem: function(obj) {
+    return new Promise((resolve, reject) => {
+      let output = {};
+      if (this._itemAlreadyAdded(obj)) {          
+        output.type = "error";
+        output.message = "ERROR: The item is already in the favorite list.";   
+      } else {           
+        let data = this.data;
+        this.data = [...data, obj];             
+        sessionStorage.setItem("Favorite", JSON.stringify(this.data));   //save to sessionStorage
+        output.type= "success";
+        output.message = "The product was added to favorite list.";
+      }  
+      output.payload = this.data;     
+      if(output.type === "success") {
+        resolve(output);
+      } else {
+        reject(output);
+      }
+    });
+  },
+  removeItem: function(obj) {    
+    return new Promise((resolve,reject) => {
+      let arr = this.data;  
+      let output = {};    
+      if((this.data).filter(o => o.id === obj.id).length > 0) {  
+          this.data = arr.filter(o => o.id !== obj.id);         
+          output.message = "TRIGGER: Product was removed from favorite list";
+          output.payload = this.data;          
+          sessionStorage.setItem("Favorite", JSON.stringify(this.data));   //save to sessionStorage
+          resolve(output);        
+      } else {
+        output.message = "ERROR: The selected product is not in the favorite list";
+        output.payload = this.data;       
+        reject(output);
+      }      
+    });      
+  },
+  _loadSessionStorageData: function() {  
+      let data = [];  
+      if (sessionStorage.getItem("Favorite") !== null) {
+        this.data = data.concat(JSON.parse(sessionStorage.getItem("Favorite")));
+      }      
+  },
+  init: function() {
+    // sessionStorage.clear();
+    return new Promise((resolve,reject) => {
+      this._loadSessionStorageData();
+      let payload = this.data;  
+      resolve(payload);    
+    });      
+  },    
+}
